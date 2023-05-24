c43062e881205d73ac5d7fdf026c7b1fc5365c67
RushikeshHalle <rushikesh.halle@thoughtworks.com>
2023-05-24 15:29:53 +0530

    add performance testing for a million nodes

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ use std::path::Path;
 use btree::node_type::{Key, KeyValuePair, NodeType, Offset};
 use btree::error::Error;
 use std::mem::size_of;
+use std::os::raw::c_double;
 use std::thread::Thread;
 use cpu_time::{ProcessTime,ThreadTime};
-use std::time::Duration;
+use std::time::{Duration, SystemTime};
 
 fn main() ->  Result<(), Error> {
     // Initialize a new BTree;
@@ -16,37 +17,33 @@ fn main() ->  Result<(), Error> {
         .b_parameter(100)
         .build()?;
     {
-        let wall_clock_stamp_before = ProcessTime::now();
+        let wall_clock_stamp_before = SystemTime::now();
         let cpu_clock_stamp_before = ThreadTime::now();
 
-        for i in 0..=200000 {
+        for i in 0..=1000000 {
             btree.insert(KeyValuePair::new(format!("k{}", i), format!("v{}", i)));
         }
 
-        let elapsed_wall_clock_time: Duration = wall_clock_stamp_before.elapsed();
+        let elapsed_wall_clock_time: Duration = wall_clock_stamp_before.elapsed().unwrap();
         let elapsed_cpu_clock_time: Duration = cpu_clock_stamp_before.elapsed();
         println!("insertion:");
         println!("elapsed_wall_clock_time : {:?}", elapsed_wall_clock_time);
         println!("elapsed_cpu_clock_time  : {:?}", elapsed_cpu_clock_time);
     }
     {
-        let wall_clock_stamp_before = ProcessTime::now();
+        let wall_clock_stamp_before = SystemTime::now();
         let cpu_clock_stamp_before = ThreadTime::now();
 
-        for i in 0..=200000 {
+        for i in 0..=1000000 {
             btree.search(format!("k{}", i));
         }
 
-        let elapsed_wall_clock_time: Duration = wall_clock_stamp_before.elapsed();
+        let elapsed_wall_clock_time: Duration = wall_clock_stamp_before.elapsed().unwrap();
         let elapsed_cpu_clock_time: Duration = cpu_clock_stamp_before.elapsed();
         println!("search:");
         println!("elapsed_wall_clock_time : {:?}", elapsed_wall_clock_time);
         println!("elapsed_cpu_clock_time  : {:?}", elapsed_cpu_clock_time);
     }
-    // Read it back.
-    let mut kv = btree.search("k12345".to_string())?;
-    assert_eq!(kv.key, "k12345");
-    assert_eq!(kv.value, "v12345");
 
     Ok(())
 }
